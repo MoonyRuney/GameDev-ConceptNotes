cbedc0800b362f602b6590dd8e4984070b9641ec
MoonyRuney <nmcguireable@gmail.com>
2025-09-29 03:06:01 +0100

    fixing links

--- javaScript/navbar.js
+++ javaScript/navbar.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Determine if we're in a subfolder (like systems)
+  const isInSubfolder = window.location.pathname.includes('/systems/');
+  const prefix = isInSubfolder ? '../' : '';
   const navHTML = `
     <header>
-      <h1><a href="/index.html">Concept Wiki</a></h1>
+      <h1><a href="${prefix}index.html">Concept Wiki</a></h1>
       <nav>
         <ul class="nav-bar">
-          <li><a href="/systems.html">Systems</a></li>
-          <li><a href="/world-building.html">World Building</a></li>
-          <li><a href="/entities.html">Entities</a></li>
-          <li><a href="/sitemap.html">Sitemap</a></li>
+          <li><a href="${prefix}systems.html">Systems</a></li>
+          <li><a href="${prefix}world-building.html">World Building</a></li>
+          <li><a href="${prefix}entities.html">Entities</a></li>
+          <li><a href="${prefix}sitemap.html">Sitemap</a></li>
         </ul>
       </nav>
     </header>
